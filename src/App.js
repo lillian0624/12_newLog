aa9dcdf17a011aaeb903e16c71579be51ff37584
Lillian Yang <nopeyangli@163.com>
2022-07-06 22:56:27 +1000

    initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/*类组件
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <div>I am a module</div>;
+  }
+}
+*/
+import Logs from "./Components/Logs/Logs";
+import Card from "./Components/UI/Card/Card";
+
+const App = () => {
+  return;
+  <div>
+    <Card>hhkkkkh</Card>
+    <Logs />
+  </div>;
+};
+
+export default App;
